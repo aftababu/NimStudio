d473ef0095fe94d059299748911d7a21b87e3d62
aftababu <aftababu54@gmail.com>
2026-06-10 21:36:52 +0600

    testing production 8

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,11 +2,13 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 import path from "path";
-
-const dbPath = path.resolve(process.cwd(), "packages/db/database.sqlite");
+const dbPath = path.resolve(process.cwd(), "../../packages/db/database.sqlite");
 
 const dbUrl = process.env.DATABASE_URL || `file:${dbPath}`;
 
+console.log("cwd =", process.cwd());
+console.log("dbPath =", dbPath);
+console.log("dbUrl =", dbUrl);
 const client = createClient({ url: dbUrl });
 export const db = drizzle(client, { schema });
 
